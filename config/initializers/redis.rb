6b63259fc41b191dc4902ba86b6e4a914fdf736b
mutalenic <nicomutale@gmail.com>
2025-01-27 16:53:38 +0200

    fix linters

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,7 +1,7 @@
-require 'redis'
+require "redis"
 
 begin
-  redis_url = ENV.fetch('REDIS_URL', 'redis://localhost:6379/0')
+  redis_url = ENV.fetch("REDIS_URL", "redis://localhost:6379/0")
   $redis = Redis.new(
     url: redis_url,
     ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
