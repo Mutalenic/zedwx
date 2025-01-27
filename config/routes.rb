fc5b1e2ac0112a307243ec8d2946a5a8dc71f268
mutalenic <nicomutale@gmail.com>
2025-01-27 11:15:29 +0200

    add real wx intergration

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
-namespace :api do
-  namespace :v1 do
-    get 'weather/current', to: 'weather#current'
+Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      get 'weather/current', to: 'weather#current'
+    end
   end
 end
