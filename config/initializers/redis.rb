064490277565aa3bf786a7ec17ab26e88fb40779
mutalenic <nicomutale@gmail.com>
2025-01-27 22:13:46 +0200

    update redis config

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,11 +1,8 @@
-require "redis"
+require 'redis'
 
 begin
-  redis_url = ENV.fetch("REDIS_URL", "redis://localhost:6379/0")
-  $redis = Redis.new(
-    url: redis_url,
-    ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
-  )
+  redis_url = ENV.fetch('REDIS_URL', 'redis://localhost:6379/0')
+  $redis = Redis.new(url: redis_url)
   $redis.ping
 rescue Redis::CannotConnectError => e
   Rails.logger.error "Failed to connect to Redis: #{e.message}"
