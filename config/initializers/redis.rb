b82d9ad7a93b92dc61215baf38afd02ec51a6cb3
mutalenic <nicomutale@gmail.com>
2025-01-27 16:24:58 +0200

    Add render.yaml

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,12 +1,12 @@
-require "redis"
+require 'redis'
 
 begin
   $redis = Redis.new(
-    host: ENV.fetch("REDIS_HOST", "localhost"),
-    port: ENV.fetch("REDIS_PORT", "6379")
+    url: ENV.fetch('REDIS_URL'),
+    ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
   )
   $redis.ping
-  Rails.logger.info "Connected to Redis"
 rescue Redis::CannotConnectError => e
   Rails.logger.error "Failed to connect to Redis: #{e.message}"
+  $redis = nil
 end
